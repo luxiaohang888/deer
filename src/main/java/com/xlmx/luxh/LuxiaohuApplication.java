20f25c289ee840f34d39a628e1c63eb91e0f377f
luxiaohu <983372273@qq.com>
2019-04-26 12:41:28 +0800

    【新增】搭建项目框架

---
+++ src/main/java/com/xlmx/luxh/LuxiaohuApplication.java
@@ -0,0 +1,13 @@
+package com.xlmx.luxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxiaohuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxiaohuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xlmx/luxh/LuxiaohuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xlmx.luxh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LuxiaohuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
